9d789571e7a8716a531b06456bac2c9e6f030c12
Raz Dvora <razdvora232@gmail.com>
2022-01-28 18:21:03 +0200

    Add files via upload
    scoreboard class, create scoreboard then put the text in the right position, a method that let us keep tracking the user score and another method that prints out GAME OVER when the game is done:)

---
+++ scoreboard.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+ALIGNMENT ="center"
+FONT = ("Courier",24,"normal")
+
+class Scoreboard(Turtle):
+    def __init__(self): 
+        super().__init__()
+        self.score=0
+        self.color("white")
+        self.penup()
+        self.goto(0, 260)
+        self.upadte_scoreboard()
+        self.hideturtle()
+
+    def upadte_scoreboard(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score+=1
+        self.clear()
+        self.upadte_scoreboard()
